eeaf0375c065fe06bc7a12c277773a9cd266a317
agent1red <kevinhudson@Kevins-MacBook-Pro.local>
2017-05-30 00:17:01 -0700

    Setting buttons to swift code completing the code for math equations. Added number formatter for comma seperator. added reset to label text after a certain made is selected.

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -15,6 +15,7 @@ enum modes {
     case not_set;
     case addition;
     case subtraction;
+    case multiplication;
 }
 
 
@@ -42,19 +43,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     };
     @IBAction func didPressPlus(_ sender: Any) {
+        changeMode(newMode: .addition);
     }
     @IBAction func didPressSubtract(_ sender: Any) {
+        changeMode(newMode: .subtraction);
     }
 
+    @IBAction func didPressMultiply(_ sender: Any) {
+        changeMode(newMode: .multiplication);
+    }
     @IBAction func didPressEquals(_ sender: Any) {
+        
+        guard let labelInt:Int = Int(labelString) else {
+            return;
+        }
+        if (currentMode == .not_set || lastButtonWasMode) {
+            return;
+        }
+        if (currentMode == .addition) {
+            savedNum += labelInt;
+        }
+        else if (currentMode == .subtraction) {
+            savedNum -= labelInt;
+        }
+        else if (currentMode == .multiplication) {
+            savedNum *= labelInt;
+        }
+        
+        
+        currentMode = .not_set;
+        labelString = "\(savedNum)";
+        updateText();
+        lastButtonWasMode = true;
+        
     }
     
     @IBAction func didPressClear(_ sender: Any) {
+        
+         labelString = "0";
+         currentMode = .not_set;
+         savedNum = 0;
+         lastButtonWasMode = false;
+         label.text = "0";
     }
     
     @IBAction func didPressNumber(_ sender: UIButton) {
         
         let stringValue:String? = sender.titleLabel?.text;
+        
+        if (lastButtonWasMode) {
+            
+            lastButtonWasMode = false;
+            labelString = "0";
+        }
+        
         labelString = labelString.appending(stringValue!);
         updateText();
         
@@ -66,10 +108,25 @@ class ViewController: UIViewController {
         guard let labelInt:Int = Int(labelString) else {
             return;
         }
-        label.text = "\(labelInt)";
+        
+        if (currentMode == .not_set) {
+            savedNum = labelInt;
+        }
+        let formatter:NumberFormatter = NumberFormatter();
+        formatter.numberStyle = .decimal;
+        let num:NSNumber = NSNumber(value: labelInt);
+        label.text = formatter.string(from: num);
     }
     
     func changeMode(newMode:modes) {
+        
+        if (savedNum == 0 ) {
+            return;
+        }
+        
+        currentMode = newMode;
+        lastButtonWasMode = true;
+        
         
     }
     
