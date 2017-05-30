c38d6b31729b667a107fcba1a5fe0a0a91a4882a
agent1red <kevinhudson@Kevins-MacBook-Pro.local>
2017-05-29 17:03:11 -0700

    Adding initial variables and methods for the calculator

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//modes is short for calculator modes as in addition or subtraction mode
+//enum mode section ---->
 
+enum modes {
+    case not_set;
+    case addition;
+    case subtraction;
+}
+
+
+class ViewController: UIViewController {
+    // initialize the variables at start of program (what they will be set to) 
+    
+    var labelString: String = "0";
+    var currentMode:modes = .not_set;
+    var savedNum:Int = 0;
+    var lastButtonWasMode:Bool = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+    
+    };
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    };
+
+    
+    
+    func updatetext() {
+        <#function body#>
     }
+    
+    func changeMode(newMode:modes) {
+        <#function body#>
+    }
+    
+    
+    
 
-
-}
+};
 
